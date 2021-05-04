2163583b1cdcf5dd686b904af0e0cd2a3b1b6324
36kyo <kyotar@gmail.com>
2021-05-04 18:45:36 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showIcon(IconNames.EigthNote)
+basic.pause(1000)
+basic.clearScreen()
+basic.showString(":Famima")
+basic.pause(1000)
+music.setVolume(255)
+let tempo = 120
+music.playMelody("A F C F G C5 C5 - ", tempo)
+music.playMelody("G A G C F F F - ", tempo)
+basic.forever(function () {
+	
+})
